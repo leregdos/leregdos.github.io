b9b3fbe38c2b6157c36939d04a76535cf389fd76
Sonny Mandakhnaran <sm8694@nyu.edu>
2024-02-21 16:52:55 +0400

    Add a SVG demo website

---
+++ svg-website/script.js
@@ -0,0 +1,17 @@
+let red = document.getElementById("cloudRed");
+let purple = document.getElementById("cloudPurple");
+let yellow = document.getElementById("cloudYellow");
+let green = document.getElementById("cloudGreen");
+
+red.addEventListener("click", () => {
+  alert("You have selected a red cloud");
+});
+purple.addEventListener("click", () => {
+  alert("You have selected a purple cloud");
+});
+yellow.addEventListener("click", () => {
+  alert("You have selected a yellow cloud");
+});
+green.addEventListener("click", () => {
+  alert("You have selected a green cloud");
+});
