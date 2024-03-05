7058228508b97d8ced30bdd74d2e4791a5abc513
Sonny Mandakhnaran <sm8694@nyu.edu>
2024-03-06 01:16:59 +0400

    Add sound files

---
+++ interactive-webcomic/6/script.js
@@ -0,0 +1,22 @@
+// Create the Audio object once
+const audio = new Audio("../assets/maya.mp3");
+
+// Access the sound button by ID
+const soundButton = document.getElementById("sound-button");
+
+// Access the play and mute icons by ID
+const playIcon = document.getElementById("sound-icon-play");
+const muteIcon = document.getElementById("sound-icon-mute");
+
+function toggleSound() {
+  // Check if the audio is paused
+  if (audio.paused) {
+    audio.play();
+    playIcon.style.display = "flex";
+    muteIcon.style.display = "none";
+  } else {
+    audio.pause();
+    playIcon.style.display = "none";
+    muteIcon.style.display = "flex";
+  }
+}
